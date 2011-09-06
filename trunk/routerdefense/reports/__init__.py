a17c82c0459a06cee3d50a0ed3831872e3838364
fropert@packetfault.org <fropert@packetfault.org@b5ff8ee4-6d89-6de4-4545-51adc37e19b6>
2011-09-06 22:16:51 +0000

    stdout output is borked

--- trunk/routerdefense/reports/__init__.py
+++ trunk/routerdefense/reports/__init__.py
@@ -1,4 +1,11 @@
+import inspect
+import __builtin__
+
+from routerdefense.common import *
+from routerdefense.metrics import *
+
 from main import *
+from stdout_reporting import *
 from pdf_reporting import *
 from html_reporting import *
 from csv_reporting import *
